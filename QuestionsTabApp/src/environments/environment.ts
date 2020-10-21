49940cd91ce198988423df2ea8f6d44cc60863a7
Eaindrae <50760339+Eaindrae@users.noreply.github.com>
2020-10-21 18:40:31 +0630

    Update environment.ts
    1

--- QuestionsTabApp/src/environments/environment.ts
+++ QuestionsTabApp/src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotdemotesting.azurewebsites.net/api/Request/",
-    selfUrl: "https://qbotdemotesting-questions.azurewebsites.net",
+    apiBaseUrl: "https://myanmardevelopingteam.azurewebsites.net/api/Request/",
+    selfUrl: "https://myanmardevelopingteam-questions.azurewebsites.net",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
-        tenantId: "7520da05-6887-4d9d-9876-45bbf886ac78",
-        clientId: "62a0eea7-3c79-450e-8083-ab31364d45a3",
+        tenantId: "1af8b4c7-8e05-466b-ba3b-a0ada1e909a9",
+        clientId: "beab0405-4300-4466-95d4-be7f2aadfb5a",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
